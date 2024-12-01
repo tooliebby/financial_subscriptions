e0c6c1fc84f415cb9db53d7e14cfd27bf3d4582f
tooliebby <125654433+tooliebby@users.noreply.github.com>
2024-12-01 17:42:02 +0700

    Update generate_changelog.sh

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Получение последнего тега
-last_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+# Получаем последний тег
+last_tag=$(git describe --tags --abbrev=0)
 
-# Получение коммитов с момента последнего тега
-commits=$(git log --pretty=format:"* %s [%h]" "$last_tag"..HEAD)
+# Получаем коммиты с момента последнего тега
+commits=$(git log --pretty=format:"* %s [%h]" $last_tag..HEAD)
 
-# Формирование даты
-date=$(date +"%Y-%m-%d")
-
-# Формирование заголовка
+# Генерируем заголовок для changelog
 version=$(git describe --tags --always)
-header="## ${version} - ${date}"
+date=$(date +%Y-%m-%d)
+echo "## $version - $date" >> changelog.md
 
-# Создание новой записи в changelog.md
-echo "$header" >> changelog.md
+# Добавляем коммиты в changelog
 echo "$commits" >> changelog.md
+
+# Добавляем ссылку на GitHub (адаптируйте под свой репозиторий)
 echo "" >> changelog.md
+echo "Полный лог изменений: [https://github.com/<your_username>/<your_repo>/commits/$version](https://github.com/<your_username>/<your_repo>/commits/$version)" >> changelog.md
